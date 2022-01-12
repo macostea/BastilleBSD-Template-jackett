17d1f7ac5a6416cd94f20c19f5cee3e419788ac7
Mihai Costea <macostea@users.noreply.github.com>
2022-01-12 22:54:05 +0200

    Use official jackett pkg

--- root/bootstrap-jackett.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-cd /tmp
-curl -L -o jackett.pkg "https://github.com/Thefrank/freebsd-port-sooners/releases/download/20211111/jackettdotnet-0.19.228.pkg"
-pkg install -y jackett.pkg
-
-rm -rf /tmp/jackett.pkg
